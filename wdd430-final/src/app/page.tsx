4e54ca43510b6bee1f0b097e171bb8e46779e7f9
kylebowser <kylebowser9@gmail.com>
2026-07-28 20:07:08 -0600

    Home page base

---
+++ wdd430-final/src/app/page.tsx
@@ -0,0 +1,18 @@
+// import AcmeLogo from '@/app/ui/acme-logo';
+// import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+//import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col p-6">
+      <div className="flex flex-col justify-center gap-6 bg-lime-800 px-6 py-10 w-full min-h-30">
+        <p className="flex text-left pl-20 text-2xl font-medium text-white">
+          HandCrafted Haven
+        </p>
+      </div>
+      
+    </main>
+  );
+}
---
+++ wdd430-final/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: "#954535",
+        olive: "#8DA054",
+      },
+    },
+  },
+  plugins: [],
+};
