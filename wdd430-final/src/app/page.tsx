22e6a05c9bb37642cbc1bd87e864ab562cdbf336
kylebowser <kylebowser9@gmail.com>
2026-08-06 17:00:54 -0600

    Base updates

--- wdd430-final/src/app/page.tsx
+++ wdd430-final/src/app/page.tsx
@@ -3,8 +3,13 @@
 import Link from 'next/link';
 //import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
+import Search from '@/app/ui/search';
 
-export default function Page() {
+export default async function Page(props: { searchParams?: Promise<{ query?: string; page?: string }> }) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  //const totalPages = await fetchInvoicesPages(query);
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex flex-col justify-center gap-6 bg-lime-800 px-6 py-10 w-full min-h-30">
@@ -12,6 +17,18 @@ export default function Page() {
           HandCrafted Haven
         </p>
       </div>
+      {/* <div className="mt-4 flex items-center justify-between gap-2 md:mt-8"> */}
+      {/* <div className="mt-4 flex items-center justify-between gap-2 md:mt-8 px-6">
+        <Search placeholder="Search invoices..." />
+        { <CreateInvoice />}
+      </div> */}
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+  <div className="pl-6 w-full">
+    <Search placeholder="Search invoices..." />
+  </div>
+</div>
+
+    
       
     </main>
   );
